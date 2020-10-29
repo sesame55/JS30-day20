560b4412e100ea152d194ba9e60d292e1b75226f
Sesame <mushroom0505@gmail.com>
2020-10-29 09:19:11 +0800

    3
    3

--- day20.js
+++ day20.js
@@ -10,21 +10,30 @@ let p = document.createElement('p');
 const words = document.querySelector('.words');
 words.appendChild(p);
 
+// 辨識成功就出現結果
 recognition.addEventListener('result', e => {
     // console.log(e);//每次輸入聲音都會出現
+    // 產出文字
     const transcript = Array.from(e.results)
         .map(result => result[0])
         .map(result => result.transcript)
         .join('');
 
-    const poopScript = transcript.replace(/poop|poo|shit|dump/gi, '💩');
     // 把這些發音套換成shit圖示，注意要使用 recognition.lang = 'en-US';才能正確輸入
+    const poopScript = transcript.replace(/poop|poo|shit|dump/gi, '😏');
+
+    // 輸出文字
     p.textContent = poopScript;
 
     if (e.results[0].isFinal) {
         p = document.createElement('p');
         words.appendChild(p);
     }
+});
+
+//測試-聲音停止時觸發
+recognition.addEventListener('soundend', e => {
+    console.log(e);
 });
 
 recognition.addEventListener('end', recognition.start);
